8e2edd25e25df474b40901d945152b10897c7eaf
Manjaka <manjaka.rajaonson@gmail.com>
2021-06-20 21:14:03 +0300

    updated base url

--- src/http.js
+++ src/http.js
@@ -1,7 +1,7 @@
 const url = {
 	/* base_url: "https://run.mocky.io/v3/",
 	todo: "351b3282-a540-4e4c-b72f-9f60851e93ad", */
-	base_url: "http://localhost:3001/",
+	base_url: "https://vito-todolist-back.herokuapp.com/",
 	todo: "api/v1",
 };
 
